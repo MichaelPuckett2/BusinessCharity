799474291866a3a7fe690fc2d48bea8a15bf20a2
MichaelPuckett2 <michael.puckett.ii@outlook.com>
2023-08-26 16:22:26 -0400

    updated but not right, pausing for time constraints

---
+++ Charity/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Charity;
+
+public class Manager
+{
+    public string Name { get; set; } = "My Business";
+    public ObservableCollection<Transaction> Credits { get; } = new ObservableCollection<Transaction>();
+    public ObservableCollection<Transaction> Debits { get; } = new ObservableCollection<Transaction>();
+}
+
+
+public class Transaction
+{
+    public double Value { get; set; }
+    public TransactionType TransactionType { get; set; }
+}
+
+public enum TransactionType
+{
+    None,
+    Credit,
+    Debit
+}
---
+++ CharityDesktop/ViewModel/ManagerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Charity;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CharityDesktop.ViewModel
+{
+    public class ManagerViewModel : INotifyPropertyChanged
+    {
+        private readonly Manager manager;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void Notify([CallerMemberName] string? property = null) 
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+
+        public ManagerViewModel()
+        {
+            manager = new Manager();
+        }
+
+        public ObservableCollection<Transaction> Credits => manager.Credits;
+        public ObservableCollection<Transaction> Debits => manager.Debits;
+    }
+}
